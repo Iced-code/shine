b0fba17c0f33aad52b67ad11136fcfe9cda7dc82
iced-code github <ayaanmodak1@gmail.com>
2025-07-04 01:56:21 -0400

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,129 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import base64
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+
+CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+
+def get_token():
+    auth_string = f"{CLIENT_ID}:{CLIENT_SECRET}"
+    b64_auth_string = base64.b64encode(auth_string.encode()).decode()
+
+    headers = {
+        "Authorization": f"Basic {b64_auth_string}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials"
+    }
+
+    r = requests.post("https://accounts.spotify.com/api/token", headers=headers, data=data)
+    
+    return r.json().get("access_token")
+
+@app.route('/artist/getArtist')
+def get_artist_info():
+    TOKEN = get_token()
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    params = {
+        "q": "HUNTR/X",
+        "type": "artist",
+        "limit": 1
+    }
+
+    r = requests.get(f"https://api.spotify.com/v1/search", headers=headers, params=params)
+    print(r.json())
+    artist = r.json()["artists"]["items"][0]
+    artist_id = artist["id"]
+
+    r = requests.get(f"https://api.spotify.com/v1/artists/{artist_id}", headers=headers)
+    return jsonify(r.json())
+
+
+@app.route('/track/getTrack/<track_name>')
+def get_track_info(track_name):
+    TOKEN = get_token()
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    params = {
+        "q": f"{track_name}",
+        "type": "track",
+        "limit": 1
+    }
+
+    r = requests.get(f"https://api.spotify.com/v1/search", headers=headers, params=params)
+    track = r.json()["tracks"]["items"][0]
+    track_id = track["id"]
+
+    r = requests.get(f"https://api.spotify.com/v1/tracks/{track_id}", headers=headers)
+    print(r.json())
+    return jsonify(r.json())
+
+
+def get_track_info_with_ID(track_ID):
+    TOKEN = get_token()
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    r = requests.get(f"https://api.spotify.com/v1/tracks/{track_ID}", headers=headers)
+    # print(r.json())
+    return r.json()
+
+@app.route('/album/getAlbum/<album_name>')
+def get_album_info(album_name):
+    TOKEN = get_token()
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    params = {
+        "q": f"{album_name}",
+        "type": "album",
+        "limit": 1
+    }
+
+    r = requests.get(f"https://api.spotify.com/v1/search", headers=headers, params=params)
+    album = r.json()["albums"]["items"][0]
+    album_id = album["id"]
+
+    r = requests.get(f"https://api.spotify.com/v1/albums/{album_id}", headers=headers)
+    
+    album_info = r.json()
+    album_tracks = album_info["tracks"]["items"]
+
+    tracks_info = []
+    for track in album_tracks:
+        track_ID = track["id"]
+        r_track = get_track_info_with_ID(track_ID)
+        if r_track:
+            tracks_info.append(r_track)
+
+    sorted_tracks = sorted(tracks_info, key=lambda t: t["popularity"], reverse=True)
+
+    return jsonify({
+        "album": {
+            "name": album_info["name"],
+            "release_date": album_info["release_date"],
+            "artists": album_info["artists"],
+            "images": album_info["images"], 
+            "id": album_info["id"]
+        },
+        "tracks": sorted_tracks
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/App.jsx
@@ -0,0 +1,166 @@
+import { useState, useEffect } from 'react'
+import { FaSpotify } from "react-icons/fa";
+import './App.css'
+
+
+function App() {
+  const [input, setInput] = useState("")
+  const [artist, setArtist] = useState(null);
+  const [track, setTrack] = useState(null);
+  const [tracks, setTracks] = useState(null);
+  const [album, setAlbum] = useState(null);
+  
+  const fetchArtist = async (e) => {
+    e.preventDefault();
+    if(!input.trim()) return;
+
+    setAlbum("");
+    setTracks("");
+
+    try {
+      // const res = await fetch(`http://localhost:5000/track/getTrack/${input}`);
+      const res = await fetch(`http://localhost:5000/album/getAlbum/${input}`);
+      const data = await res.json();
+      setAlbum(data.album);
+      setTracks(data.tracks);
+    } catch(err) {}
+
+    setInput("");
+  };
+
+  useEffect(() => {
+   /*  const fetchArtist = async () => {
+      // const res = await fetch(`http://localhost:5000/artist/${artistID}`);
+      const res = await fetch(`http://localhost:5000/artist/getArtist`);
+      const data = await res.json();
+      setArtist(data)
+    };
+    
+    fetchArtist(); */
+  })
+
+  return (
+    <div className='card'>
+      { (
+        <>
+          <form onSubmit={fetchArtist}>
+            <input
+              type='text'
+              placeholder='Track name'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit">Search</button>
+            {/* <button type='button' onClick={fetchArtist}>Load artist info</button> */}
+          </form>
+        </>
+      )}
+      {album && (
+        <>
+          <a href={`https://open.spotify.com/album/${album.id}`} target='_blank'>
+            <img className="albumCoverImage" src={album.images[0].url} alt="Album Cover" />
+          </a>
+          <h2>{album.name}</h2>
+          <p>{album.artists.map(a => a.name).join(", ")} Released: {album.release_date}</p>
+          <p></p>
+
+          <ul className="track-list">
+            {tracks.map((track, index) => (
+              <li key={track.id} className="track-item">
+                <div className="track-header">
+                  <div className="track-title">
+                    #{index+1}<span style={{ color: "#030339" }}>/</span><span style={{ color: "#bbb" }}>#{track.popularity}</span> {track.name}
+                  </div>
+                </div>
+
+                <div className="track-footer">
+                  <div className="track-artists">
+                    {track.artists.map((a) => a.name).join(", ")}
+                  </div>
+                  <a
+                    className="track-link"
+                    href={`https://open.spotify.com/track/${track.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaSpotify size={24}/>
+                  </a>
+                </div>
+              </li>
+
+              
+            ))}
+          </ul>
+
+          {/* <li key={track.id} className="track-item">
+                <div className="track-header">
+                  <div className="track-title">
+                    #{index + 1} / {track.name}
+                  </div>
+                  <div className="track-popularity">
+                    {track.popularity}
+                  </div>
+                </div>
+
+                <div className="track-meta">
+                  {track.artists.map(a => a.name).join(", ")}
+                </div>
+
+                <a href={`https://open.spotify.com/track/${track.id}`} target='_blank' rel='noopener noreferrer'>
+                  Listen Now
+                </a>
+              </li> */}
+
+
+          
+          {/* <ul className="track-list">
+            {tracks.map((track, index) => (
+              <li key={track.id} className="track-item">
+                <div className="track-title">
+                  <div> #{index+1}<span style={{ color: "#030339" }}>/</span><span style={{ color: "#bbb" }}>#{track.popularity}</span> </div>
+                  </div>
+                <div className="track-meta">
+                  <div className='track-title'> {track.name} </div>
+                  {track.artists.map(a => a.name).join(", ")}<br />
+                </div>
+                <a href={`https://open.spotify.com/track/${track.id}`} target='_blank'>
+                  Listen Now
+                </a>
+              </li>
+            ))}
+          </ul> */}
+        </>
+      )}
+    </div>
+  );
+
+  /* return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        
+        <iframe 
+          style={{borderRadius:"12px"}} 
+          src="https://open.spotify.com/embed/track/1CPZ5BxNNd0n0nF4Orb9JS?utm_source=generator" 
+          width="100%" 
+          height="352"
+          allowfullscreen="" 
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+          loading="lazy"
+        />
+
+        <iframe
+          style={{ borderRadius: '12px' }}
+          src="https://open.spotify.com/embed/album/14JkAa6IiFaOh5s0nMyMU9?utm_source=generator"
+          width="100%"
+          height="520"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        />
+      </div>
+    </>
+  ) */
+}
+
+export default App
